45038af83a11e7047f0d9c3bdd7d0ff668dbe837
kosaeszt <kosaeszt@gmail.com>
2025-08-27 21:35:08 +0200

    feat(db): add local Postgres connection and test endpoints (not AWS yet)

--- main.js
+++ main.js
@@ -1,35 +1,69 @@
 const express = require('express')
+require('dotenv').config();
 const app = express()
+const bodyParser = require('body-parser')
 const port = 3000
 
 app.use(express.json())
 
-app.get('/api/profile', (req, res) => {
-  res.json({
-    name:"Test Name", 
-    email: "test@test.com",
-    about: "test about"
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
   })
+)
+
+const Pool = require('pg').Pool
+
+const pool = new Pool({
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DB,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+});
+
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' })
 })
 
-app.get('/api/experience', (req, res) => {
-  res.json([
-    { company: "testcompany1", role: "Test Engineer", years: "2020-2023" },
-    { company: "testcompany2", role: "Test Engineer", years: "2018-2020" }
-  ])
-})
+app.get('/api/profile', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM profile');
+    res.json(result.rows); 
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Database error' });
+  }
+});
 
-app.post('/api/contact', (req, res) => {
+app.get('/api/experience', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM experience');
+    res.json(result.rows); 
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Database error' });
+  }
+});
+
+app.post('/api/contact', async (req, res) => {
   const { name, email, message } = req.body;
 
   if (!name || !email || !message) {
-    return res.status(400).json({ error: 'All fields required'});
+    return res.status(400).json({ error: 'All fields required' });
   }
 
-  res.status(201).json({
-    status: "success"
-  })
-})
+  try {
+    await pool.query(
+      'INSERT INTO contacts (name, email, message) VALUES ($1, $2, $3)',
+      [name, email, message]
+    );
+    res.status(201).json({ status: 'success' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Database error' });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
